3cff7abe39e43acaf99a79d5c1e462838fedb446
Nawaf alsmail <nalsmail@tree.com.sa>
2025-11-23 14:08:09 +0300

    webReactExApp JSB

---
+++ Java self projects/myFirstApp/src/main/resources/Data1.sql
@@ -0,0 +1,23 @@
+
+
+//something wrong here i think, it's not saving rightly. and i'm suspecting on here
+
+INSERT INTO Product(name, description, brand, price, category, release_date, productAvailable, stockQuantity)
+values ('Supra', 'Fast car','Toyota',100000,'Car','2020-09-09', true, 2);
+
+
+--INSERT INTO product(name, description, brand, price, category, release_date, productAvailable, stockQuantity)
+--values ('Sky Line', 'The fastest car','Nissan',1000000,'Car','2020-03-03', true, 1);
+--
+--INSERT INTO product(name, description, brand, price, category, release_date, productAvailable, stockQuantity)
+--values ('Toures', 'the most comfy car','Ford',70000,'Car','2024-04-04', true, 4);
+--
+--INSERT INTO product(name, description, brand, price, category, release_date, productAvailable, stockQuantity)
+--values ('TV', 'A wide display screen','LG',3000,'Electronics','2021-09-09', true, 7);
+--
+--
+--INSERT INTO product(name, description, brand, price, category, release_date, productAvailable, stockQuantity)
+--values ('Iphone', 'A mobile phone','Apple',2000,'Electronics','2025-03-03', true, 10);
+
+--INSERT INTO product(name, description, brand, price, category, release_date, productAvailable, stockQuantity)
+--values ('PS5', 'A video player','Sony',3000,'Electronics','2024-02-02', true, 8);
