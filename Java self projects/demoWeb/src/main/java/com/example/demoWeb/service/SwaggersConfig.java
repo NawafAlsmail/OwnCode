b506fb30ab38f810a295a416370e14c773265cde
Nawaf alsmail <nalsmail@tree.com.sa>
2025-12-14 17:29:33 +0300

    Swagger UI have been added here

--- Java self projects/demoWeb/src/main/java/com/example/demoWeb/service/SwaggersConfig.java
+++ Java self projects/demoWeb/src/main/java/com/example/demoWeb/service/SwaggersConfig.java
@@ -1,9 +1,12 @@
 package com.example.demoWeb.service;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Collections;
 
 @Configuration
 public class SwaggersConfig {
@@ -14,6 +17,14 @@ public class SwaggersConfig {
                         .title("Demo Web API")
                         .description("Self project REST APIs documentation")
                         .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Nawaf")
+                                .url("https://github.com/nawafalsmail")
+                                .email("nn@gmail.com"))
+                        .termsOfService("Free to use")
+                        .license(new io.swagger.v3.oas.models.info.License()
+                                .name("API License")
+                                .url("https://github.com/nawafalsmail"))
                 );
     }
 }
